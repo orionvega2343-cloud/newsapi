2a2379a2d95be960bf733cd4f8bb9a943b87e20a
Nesanesis <orionvega2343@gmail.com>
2026-04-21 22:19:30 +0300

    Add structs and migrations

--- internal/models.go
+++ internal/models.go
@@ -1,1 +1,18 @@
 package internal
+
+import "time"
+
+type Article struct {
+	Id        int       `db:"id"  json:"id"`
+	Title     string    `db:"title" json:"title"`
+	Link      string    `db:"link" json:"link"`
+	Source    string    `db:"source" json:"source"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+}
+
+type User struct {
+	Id           int       `db:"id" json:"id"`
+	Email        string    `db:"email" json:"email"`
+	PasswordHash string    `db:"password_hash" json:"-"`
+	CreatedAt    time.Time `db:"created_at" json:"created_at"`
+}
