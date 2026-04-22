c4e8414bd4a4a6ed4706d5ef19c9dd1d698a3b3e
Nesanesis <orionvega2343@gmail.com>
2026-04-22 15:08:29 +0300

    Added midleware

--- internal/middleware.go
+++ internal/middleware.go
@@ -1,1 +1,35 @@
 package internal
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ok := c.GetHeader("Authorization")
+		if ok == "" {
+			c.AbortWithStatus(401)
+			return
+		}
+		res := strings.TrimPrefix(ok, "Bearer ")
+
+		token, err := jwt.Parse(res, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte(os.Getenv("JWT_SECRET")), nil
+
+		})
+		_ = token
+		if err != nil {
+			c.AbortWithStatus(401)
+			return
+		}
+		c.Next()
+	}
+}
