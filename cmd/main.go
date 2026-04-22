64b4bf8ea4dc9fa885555d3b00da1b550fc3ab3c
Nesanesis <orionvega2343@gmail.com>
2026-04-22 16:53:08 +0300

    Added starting server and routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,37 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"fmt"
+	"log"
+	"newsapi/internal"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	db, err := internal.NewDB(connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	repo := internal.NewRepository(db)
+	svc := internal.NewService(repo)
+	res := internal.NewHandler(svc)
 
+	r := gin.Default()
+
+	r.POST("/auth/login", res.Login)
+	r.POST("/auth/register", res.Register)
+	authorized := r.Group("/")
+	authorized.Use(internal.AuthMiddleware())
+	authorized.GET("/news", res.GetArticle)
+	r.Run(os.Getenv("DB_PORT"))
+
+}
